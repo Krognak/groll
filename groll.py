cd5bcdc1bf9c6443bb0ea6f0deb171ab5db8db23
Gage <gagetalbot@pm.me>
2022-07-16 17:47:31 +0100

    a fix for sticky arguments

--- groll.py
+++ groll.py
@@ -15,8 +15,10 @@ __version__ = "0.1.0"
 
 logging.basicConfig(level=logging.DEBUG)
 
-DICE_SYNTAX = r"(?P<pre>[\+-/\*]?)(?P<die>\d*d\d+)(?P<post>[\+-/\*]?)"
-MOD_SYNTAX = r"(?P<pre>[\+-/\*]?)(?P<mod>\d+)(?P<post>[\+-/\*]?)"
+re.DOTALL = True
+
+DICE_SYNTAX = r"\d*d\d+"
+MOD_SYNTAX = r"\d+"
 
 OPS = {
     "+": lambda x, y: x + y,
@@ -44,13 +46,7 @@ def sub(args: list, pattern: str, func: Callable) -> list:
     for arg in args:
         m = re.match(pattern, arg)
         if m:
-            pre, target, post = m.groups()
-            print(pre, target, post)
-            if pre:
-                subbed.append(pre)
-            subbed.append(func(target))
-            if post:
-                subbed.append(post)
+            subbed.append(func(m.group(0)))
         else:
             subbed.append(arg)
     return subbed
@@ -66,8 +62,10 @@ def eval_roll(args: list) -> int:
 
 
 def cli() -> None:
-    args = sys.argv[1:]
-    logging.info(f"args supplied = {args}")
+    args = " ".join(sys.argv[1:])
+    args = re.sub(r"[\+-/\*]", lambda x: f" {x.group(0)} ", args)
+    args = args.split()
+    logging.info(f"tidied args = {args}")
     args = sub(args, DICE_SYNTAX, roll)
     logging.info(f"with dice subbed = {args}")
     args = sub(args, MOD_SYNTAX, int)
